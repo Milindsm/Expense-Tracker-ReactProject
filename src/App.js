b01781ef3dfbe8a02951b21420fab09aeb47c65b
Milindsm <milu.mangale@gmail.com>
2023-03-12 13:16:26 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,58 @@
 import ExpenseItem from "./components/ExpenseItem";
 function App() {
+  const expense = [
+    {
+      title: "Toilet Paper",
+      amount: 50,
+      date: new Date(2023, 2, 12),
+      location: "Pune",
+    },
+    {
+      title: "Toilet Paper",
+      amount: 50,
+      date: new Date(2023, 2, 12),
+      location: "Pune",
+    },
+    {
+      title: "Toilet Paper",
+      amount: 50,
+      date: new Date(2023, 2, 12),
+      location: "Pune",
+    },
+    {
+      title: "Toilet Paper",
+      amount: 50,
+      date: new Date(2023, 2, 12),
+      location: "Pune",
+    },
+  ];
   return (
     <div>
       <h2>Let's Start</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expense[0].title}
+        amount={expense[0].amount}
+        date={expense[0].date}
+        location={expense[0].location}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expense[1].title}
+        amount={expense[1].amount}
+        date={expense[1].date}
+        location={expense[1].location}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expense[2].title}
+        amount={expense[2].amount}
+        date={expense[2].date}
+        location={expense[2].location}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expense[3].title}
+        amount={expense[3].amount}
+        date={expense[3].date}
+        location={expense[3].location}
+      ></ExpenseItem>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,18 @@
-function ExpenseItem() {
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+    const expenseDate = new Date(2023, 2, 12);
+    const expenseTitle = 'Rent';
+    const expenseAmount = 2000;
+    const expenselocation = 'Pune'
   return (
-    <div>
-        <h2>Expense Items</h2>
-        <div>
-            <div>Food Rs.10</div>
-            <div>Petrol Rs.100</div>
-            <div>Movie Rs.200</div>
-            </div>
+    <div className='expense-item'>
+        <div>{props.date.toISOString()}</div>
+        <div className='expense-item__description'>
+            <h2>{props.title}</h2>
+            <div className='expense-item__price'>{props.amount}</div>
+            <div  >{props.location}</div>
+        </div>
     </div>
   
   );
