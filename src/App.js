9682a15937e77db7738485dffbabaceb49143e83
Milindsm <milu.mangale@gmail.com>
2023-03-12 10:37:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import ExpenseItem from "./components/ExpenseItem";
+function App() {
+  return (
+    <div>
+      <h2>Let's Start</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+function ExpenseItem() {
+  return (
+    <div>
+        <h2>Expense Items</h2>
+        <div>
+            <div>Food Rs.10</div>
+            <div>Petrol Rs.100</div>
+            <div>Movie Rs.200</div>
+            </div>
+    </div>
+  
+  );
+}
+
+export default ExpenseItem;
